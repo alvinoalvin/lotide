e838fafc105f32afd613dc66352a8d4b19eb3a38
Alvin Ng <alvinoalvin123@gmail.com>
2021-03-29 21:59:21 +0000

    tail.js

--- tail.js
+++ tail.js
@@ -14,7 +14,7 @@ const tail = function (array) {
   return rtnArr;
 };
 
-// Test Case: Check the original array
+// Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 console.log(tail(words)); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
