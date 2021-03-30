49b5ae4cd4bc6bcd2048fd6201c8b033fe64022a
Alvin Ng <alvinoalvin123@gmail.com>
2021-03-30 23:11:18 +0000

    asserArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+/* Function that checks if 2 values */
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/* Function that checks if 2 arrays are equal */
+const eqArrays = function (actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (!assertEqual(actual[i], expected[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/* Checks if 2 arrays are equal and return a message */
+const assertArraysEqual = function () {
+  if (eqArrays) {
+    console.log("PASSED: " + actual + "===" + expected);
+  }
+  else { console.log("Failed: " + actual + "!==" + expected)}
+}
--- eqArrays.js
+++ eqArrays.js
@@ -19,3 +19,4 @@ console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));             // =
 console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));             // => false
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));   // => false
+
