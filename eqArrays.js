2da3b8ca57954e20c098f6c7ed218badf4ae914f
Alvin Ng <alvinoalvin123@gmail.com>
2021-03-30 22:43:53 +0000

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqArrays = function (actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (!assertEqual(actual[i], expected[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));             // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));             // => false
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));   // => false
