0f75348fd02d8da6cc699f00a922c1ab7660aee7
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-02 05:40:51 +0000

    lotide_updates

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,10 @@
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
