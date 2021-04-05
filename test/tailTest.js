1502081032819374321a8251cb10222a6a502912
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-05 22:34:24 +0000

    index.js shortened

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,15 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
+
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-assertArraysEqual(tail(words), ["Lighthouse", "Labs"]); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it(`returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+
+  it(`returns length of 3 after run (original not modified)`, () => {
+    assert.deepEqual(words.length, 3);
+  });
+});
